a01967a83ef8b0f26bc39b47213f234d3926bd31
Gurbuxsinghani <bgurbuxsinghani@deloitte.com>
2025-06-11 22:31:09 -0400

    commiting another py program file

---
+++ Python_Code/optimized_if_else_condition.py
@@ -0,0 +1,11 @@
+# optimized if_else_condition
+
+x = int(input('enter number for odd even test'))
+r = x % 2
+
+if r == 0:
+    print("it's even")
+else:
+    print("it's odd!")
+
+print('end of program!')
