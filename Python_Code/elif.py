a2e89c780687eeb729feeb16d323bdf079eae7a2
Gurbuxsinghani <bgurbuxsinghani@deloitte.com>
2025-06-11 22:43:35 -0400

    adding another py program

---
+++ Python_Code/elif.py
@@ -0,0 +1,14 @@
+# elif program
+# print in words the value of number
+x = int(input('input number between 1 to 4'))
+
+if x == 1:
+    print('one')
+elif x == 2:
+    print('two')
+elif x ==3:
+    print('three')
+elif x ==4:
+    print('four')
+else:
+    print('it is beyond 4!!')
