177c60a8b95bdfb6b79dbcd52d4343daeb69e3f9
Gurbuxsinghani <bgurbuxsinghani@deloitte.com>
2025-06-11 22:23:25 -0400

    adding first python code file

---
+++ Python_Code/if_else_condition.py
@@ -0,0 +1,10 @@
+# program to understand if clause and if elif clause
+
+x = int(input('enter a number to test odd v/s even'))
+r = x % 2
+
+if(r == 0):
+    print("it/'s even")
+if(r != 0):
+    print("it's odd!")
+print('end of program!')
